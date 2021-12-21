31eba826a9dbba3cc24af94ce79507e22ed29da4
Ryan Archer <rlarcher@andrew.cmu.edu>
2021-12-21 15:49:56 -0500

    Remove the fake latency from the backend server. Add CORS handling to allow requests from the localhost browser. Add random failures to all endpoints

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,18 @@ function shouldFail() {
     return false;
 }
 
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT");
+    next();
+  });
+
 app.get('/get-random-todo', (_, res) => {
+    if (shouldFail()) {
+        res.sendStatus(503);
+        return;
+    }
     const randomIndex = Math.floor(Math.random() * randomTodos.length);
     const todo = randomTodos[randomIndex];
     res.json({
@@ -24,21 +35,32 @@ app.get('/get-random-todo', (_, res) => {
 app.post('/add-todo', (req, res) => {
     if (shouldFail()) {
         res.sendStatus(503);
+        return;
     }
 
     const item = req.body.todo;
-    const randomTimeoutAmount = Math.random() * 5000;
-    setTimeout(() => {
-        localTodos.push(item);
-        res.sendStatus(200);
-    }, randomTimeoutAmount);
+    localTodos.push(item);
+    res.json({
+        todos: localTodos,
+    });
 });
 
 app.get('/get-todos', (_, res) => {
     if (shouldFail()) {
         res.sendStatus(503);
+        return;
     }
 
+    res.json({
+        todos: localTodos,
+    });
+});
+
+app.get('/remove-todo', (_, res) => {
+    if (shouldFail()) {
+        res.sendStatus(503);
+    }
+    localTodos.pop();
     res.json({
         todos: localTodos,
     });
